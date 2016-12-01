9b0bf00c82254a2fc102cbc46b2c4b3fb54d980d
Joel Wells <buzwells@gmail.com>
2016-12-01 07:53:54 -0600

    improved bug samples

--- threads/thread-bug2.c
+++ threads/thread-bug2.c
@@ -1,32 +1,13 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #define NUM_THREADS 5
 
-int sum = 0;
-pthread_mutex_t common_mutex;
 long tasks[NUM_THREADS];
 
 // function to work in a thread
 void *worker_thread(void *arg) {
-  printf("This is worker_thread #%ld\n", (long)arg);
-  for(int a = 0; a < 5; a++) {
-    if( sum < 15 ) {
-      sleep(1);
-      printf("incrementing sum\n");
-      int ret = pthread_mutex_lock(&common_mutex);
-      if(ret == EINVAL) {
-        printf("Encountered an error trying to lock mutex\n");
-        pthread_exit(NULL);
-      }
-      sum++;
-      pthread_mutex_unlock(&common_mutex);
-    } else {
-      printf("Cap is reached in work thread #%ld\n", (long) arg);
-    }
-  }
+  printf("This is worker_thread #%ld\n", (long) arg);
   pthread_exit(NULL);
 }
 
@@ -38,18 +19,14 @@ int main() {
 
   // create threads       
   long id;
-  for (id = 0; id < 5; id++) {
+  for (id = 1; id <= 5; id++) {
+    // using this array ensures that the thread will access a stable value safely
     tasks[id] = id;
     int ret = pthread_create(&my_thread[id], NULL, &worker_thread, (void*) tasks[id]);
   }
 
-  for (id = 0; id < 5; id++) {
-    pthread_join(my_thread[id], NULL);
-    printf("thread %ld is done\n", id);
-  }
 
-  printf("main program done - sum = %d\n", sum);
-  //close main thread
-  pthread_exit(NULL);
+  printf("main program done\n");
+  exit(EXIT_SUCCESS);
 }
 
